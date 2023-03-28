91759bdbb5efdc2d68f88a576a1344fd8ef456c8
Yakub Ajibade Adebare <ajibadeyakubadebare@gmail.com>
2023-03-28 07:33:28 +0100

    f

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -84,6 +84,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
